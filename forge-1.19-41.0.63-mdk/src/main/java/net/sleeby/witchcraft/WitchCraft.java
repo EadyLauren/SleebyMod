b267e24f992ff428ff76316bb388debc872f1f11
EadyLauren <lauren_eady@hotmail.co.uk>
2023-03-02 17:30:30 +0000

    Initial commit

---
+++ forge-1.19-41.0.63-mdk/src/main/java/net/sleeby/witchcraft/WitchCraft.java
@@ -0,0 +1,48 @@
+package net.sleeby.witchcraft;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(WitchCraft.MOD_ID)
+public class WitchCraft
+{
+
+    public static final String MOD_ID = "witchcraft";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public WitchCraft()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+
+    
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
